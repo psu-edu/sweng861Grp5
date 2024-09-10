75263c7b3ff734a7dfb226f00e1122229bdc4d62
Josh Burgess <burgessj247@gmail.com>
2024-09-09 20:37:53 -0400

    feat: adds eslint rules and adjusts configs to include formatter into the linter

---
+++ app/eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import jest from "eslint-plugin-jest";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  { ignores: ["dist/"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["tests/**/*.{js,ts}"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off"
+    }
+  },
+  eslintPluginPrettierRecommended
+];
