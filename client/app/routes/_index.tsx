81ba3b33779216ada545fe9fca8fa3d73df776fa
Josh Burgess <burgessj247@gmail.com>
2024-09-18 13:11:41 -0400

    chore: lints client

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -2,16 +2,16 @@ import { title } from "@/config.shared";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: title() },
-    { name: "description", content: "Welcome to BitFit!" },
-  ];
+	return [
+		{ title: title() },
+		{ name: "description", content: "Welcome to BitFit!" },
+	];
 };
 
 export default function Index() {
-  return (
-    <main className="container prose py-8">
-      <h1 className="text-5xl">BitFit</h1>
-    </main>
-  );
+	return (
+		<main className="container prose py-8">
+			<h1 className="text-5xl">BitFit</h1>
+		</main>
+	);
 }
