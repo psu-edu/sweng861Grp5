44364a2b8598f814e7128ab2d3b5357ac2f56c99
Josh Burgess <burgessj247@gmail.com>
2024-09-18 13:25:00 -0400

    fix: formats files in client

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-	testEnvironment: "jsdom",
-	transform: {
-		"^.+\\.(js|jsx|ts|tsx)$": "@swc/jest",
-	},
-	collectCoverage: true,
-	coverageThreshold: {
-		global: {
-			branches: 70,
-			functions: 80,
-			lines: 80,
-			statements: 70,
-		},
-	},
-	collectCoverageFrom: ["./app"],
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "@swc/jest",
+    },
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            branches: 70,
+            functions: 80,
+            lines: 80,
+            statements: 70,
+        },
+    },
+    collectCoverageFrom: ["./app"],
 };
