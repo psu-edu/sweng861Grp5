f9eb1093bcdb2ce396ac7e976b78ec88c20b6729
Josh Burgess <burgessj247@gmail.com>
2024-09-18 13:08:05 -0400

    feat: creates simple UI from framework template, includes container, includes tests for ci

---
+++ client/app/routes/_index.tsx
@@ -0,0 +1,17 @@
+import { title } from "@/config.shared";
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: title() },
+    { name: "description", content: "Welcome to BitFit!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <main className="container prose py-8">
+      <h1 className="text-5xl">BitFit</h1>
+    </main>
+  );
+}
