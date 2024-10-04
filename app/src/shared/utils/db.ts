32fa137dfa68c5a506d79e52785e0f0905ce18c3
Josh Burgess <burgessj247@gmail.com>
2024-10-04 10:51:15 -0400

    DB file working - may need to make dynamic

--- app/src/shared/utils/db.ts
+++ app/src/shared/utils/db.ts
@@ -2,21 +2,21 @@ import mongoose from "mongoose";
 import { mongo } from "./constants";
 import { logger } from "./logger";
 
-export default function connectDB(dbName: string) {
-	try {
-		mongoose.connect(`mongo.connectionString/${dbName}`);
-	} catch (err: any) {
-		logger.error(err.message);
-		process.exit(1);
-	}
-	const dbConnection = mongoose.connection;
-	dbConnection.once("open", (_) => {
-		logger.info(`Database connected: ${mongo.connectionString}`);
-	});
+export default function connectDB() {
+  try {
+    mongoose.connect(mongo.connectionString);
+  } catch (err: any) {
+    logger.error(err.message);
+    process.exit(1);
+  }
+  const dbConnection = mongoose.connection;
+  dbConnection.once("open", (_) => {
+    logger.info(`Database connected: ${mongo.connectionString}`);
+  });
 
-	dbConnection.on("error", (err) => {
-		logger.error(`String ${mongo.connectionString}`);
-		logger.error(`connection error: ${err}`);
-	});
-	return;
+  dbConnection.on("error", (err) => {
+    logger.error(`String ${mongo.connectionString}`);
+    logger.error(`connection error: ${err}`);
+  });
+  return;
 }
