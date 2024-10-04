a071303a03c43cb96d995766d701dae94d2c3e24
Race Mahoney <rjm5893@psu.edu>
2024-10-04 13:26:35 -0400

    updating db and constants file

--- app/src/shared/utils/db.ts
+++ app/src/shared/utils/db.ts
@@ -1,26 +1,22 @@
 import mongoose from "mongoose";
+import { mongo } from "./constants";
 import { logger } from "./logger";
 
 export default function connectDB() {
-	const url = "mongodb://localhost:27017/";
+    try {
+        mongoose.connect(mongo.connectionString);
+    } catch (err: any) {
+        logger.error(err.message);
+        process.exit(1);
+    }
+    const dbConnection = mongoose.connection;
+    dbConnection.once("open", (_) => {
+        logger.info(`Database connected: ${mongo.connectionString}`);
+    });
 
-	try {
-		mongoose.connect(url, {
-			dbName: process.env.DB_NAME,
-			user: process.env.DB_USER,
-			pass: process.env.DB_PASS,
-		});
-	} catch (err: any) {
-		logger.error(err.message);
-		process.exit(1);
-	}
-	const dbConnection = mongoose.connection;
-	dbConnection.once("open", (_) => {
-		logger.info(`Database connected: ${url}`);
-	});
-
-	dbConnection.on("error", (err) => {
-		logger.error(`connection error: ${err}`);
-	});
-	return;
+    dbConnection.on("error", (err) => {
+        logger.error(`String ${mongo.connectionString}`);
+        logger.error(`connection error: ${err}`);
+    });
+    return;
 }
