ef4fe2bc70872309e91f68e78ecccb9fc0b8871b
Josh Burgess <burgessj247@gmail.com>
2024-10-08 08:52:53 -0400

    Creates vital backend services for user creation and token retrieval

---
+++ app/src/modules/vital/controllers/vitalController.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from "express";
+import { logger } from "../../../shared/utils/logger";
+import { VitalClient, VitalEnvironment } from "@tryvital/vital-node";
+
+class VitalController {
+  async getToken(req: Request, res: Response): Promise<void> {
+    logger.info("Exchanging userId for VitalToken...");
+    try {
+      const client = new VitalClient({
+        apiKey: process.env.VITAL_API_KEY,
+        environment: VitalEnvironment.Sandbox,
+      });
+
+      const data = await client.link.token({ userId: req.params.vitalUserId! });
+
+      res.status(200).json(data);
+      logger.info("Successfully exchanged vital token");
+    } catch (error: any) {
+      logger.error(`Error exchanging id for token: ${error.message}`);
+      res.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export default new VitalController();
